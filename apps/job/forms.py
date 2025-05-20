13a1e9fc0a65c480d55c1b6a96fa30813e0a45e4
freyben <derjayman@gmail.com>
2025-05-20 15:04:57 +0200

    Added Job model and form

---
+++ apps/job/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Job
+
+
+class AddJobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = [
+            "job_name",
+            "link",
+            "job_description",
+        ]  # Fields from the model
+
+    job_name = forms.CharField(
+        label="Job name",
+        max_length=200,
+        required=True,
+        widget=forms.TextInput(
+            attrs={"placeholder": "Enter Jobname/Position..."}
+        ),
+    )
+
+    link = forms.URLField(
+        label="Job link",
+        max_length=300,
+        required=False,
+        widget=forms.TextInput(attrs={"placeholder": "Enter link..."}),
+    )
+
+    job_description = forms.CharField(
+        label="Job description",
+        max_length=2000,
+        required=False,
+        widget=forms.Textarea(
+            attrs={"placeholder": "Enter a description..."}
+        ),
+    )
--- apps/job/models.py
+++ apps/job/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Job(models.Model):
+    # One user many jobs
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    # Is mandatory
+    job_name = models.CharField(max_length=200, null=False, blank=False)
+    link = models.URLField(
+        max_length=300,
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now=True)
+    job_description = models.TextField(
+        max_length=2000,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+
+        return f"{self.job_name}"
+
+    class Meta:
+        # app_name_model_name, user_user
+        db_table = "job"
