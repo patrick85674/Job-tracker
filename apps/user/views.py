a45ce655b7ac25870e81f83da194b8261cdca0f0
Eli <elisa.vassanelli@dci-student.org>
2025-05-23 13:50:31 +0200

    Merge main, finalize user auth (register/login/logout/password reset), and add Watchlist model with initial migration

--- apps/user/views.py
+++ apps/user/views.py
@@ -57,55 +57,6 @@ class CustomLoginView(DjangoLoginView):
             return self.form_invalid(form)
 
 
-# class CustomLoginView(DjangoLoginView):
-#     template_name = 'login.html'
-#     authentication_form = CustomLoginForm
-
-#     def post(self, request, *args, **kwargs):
-#         form = self.get_form()
-
-#         if form.is_valid():
-#             identifier = form.cleaned_data.get("identifier")
-#             password = form.cleaned_data.get("password")
-
-#             # Try authenticating with username first
-#             user = authenticate(request, username=identifier, password=password)
-
-#             if user is None:
-#                 # Try to find user by email and authenticate with username
-#                 try:
-#                     user_obj = User.objects.get(email=identifier)
-#                     user = authenticate(
-#                         request,
-#                         username=user_obj.username,
-#                         password=password
-#                     )
-#                 except User.DoesNotExist:
-#                     user = None
-
-#             if user is not None:
-#                 request.session['failed_attempts'] = 0
-#                 login(request, user)
-#                 return redirect('home')
-
-#             # Authentication failed
-#             failed_attempts = request.session.get('failed_attempts', 0) + 1
-#             request.session['failed_attempts'] = failed_attempts
-
-#             if failed_attempts >= 3:
-#                 messages.error(
-#                     request,
-#                     "Too many failed attempts. Forgot your password?"
-#                 )
-#             else:
-#                 messages.error(request, "Invalid username or password.")
-
-#         return self.form_invalid(form)
-
-#     def get_form(self, form_class=None):
-#         return self.authentication_form(self.request.POST or None)
-
-
 def logout_view(request):
     logout(request)
     return redirect('login')
