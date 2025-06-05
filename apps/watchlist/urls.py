a8ace6a4b782dfce7138f52bba879c88c901bb36
freyben <derjayman@gmail.com>
2025-06-05 13:51:26 +0200

    feat: Added delete button for watchlist

--- apps/watchlist/urls.py
+++ apps/watchlist/urls.py
@@ -8,4 +8,9 @@ app_name = "watchlist"
 urlpatterns = [
     path("partial/", views.watchlist_partial, name="watch_list_partial"),
     path("add/", views.add_job_to_watchlist, name="add_job_to_watchlist"),
+    path(
+        "watchlist_partial_remove/<int:id>",
+        views.partial_remove_view,
+        name="watchlist_partial_remove",
+    ),
 ]
--- apps/watchlist/views.py
+++ apps/watchlist/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from apps.watchlist.models import Watchlist
 from apps.job.forms import JobAddForm
 from django.http import HttpResponse
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.translation import gettext_lazy as _
+
 
 
 @login_required
@@ -56,3 +59,26 @@ def current_watchlist(request):
         "partials/watchlist_list.html",
         {"watchlist_items": watchlist_items},
     )
+
+
+@login_required
+def watchlist_remove_view(request, id):
+    watchlist = get_object_or_404(Watchlist, id=id)
+    if watchlist.user != request.user:
+        return HttpResponseForbidden(
+            _("No permission to remove this application!")
+        )
+    watchlist.delete()
+
+    context = {}
+    return render(request, "watchlist_entry_removed.html", context)
+
+
+# from django.shortcuts import render, redirect, get_object_or_404
+
+@login_required
+def partial_remove_view(request, id):
+    # Deletes item in the watchlist
+    watchlist_remove_view(request, id)
+    # Update partial list
+    return watchlist_partial(request)
