efc389710dcfd44d4dbe462816006353cd6bd0e2
freyben <derjayman@gmail.com>
2025-06-05 14:06:20 +0200

    Fix: Changed watchlist model to inherit from commons/base_models/DateColumns

--- apps/watchlist/models.py
+++ apps/watchlist/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.conf import settings
 from apps.job.models import Job
+from apps.common.models.base_models import DateColumns
 
-class Watchlist(models.Model):
+
+class Watchlist(DateColumns):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
@@ -11,4 +13,4 @@ class Watchlist(models.Model):
         Job,
         on_delete=models.CASCADE
     )
-    date_added = models.DateTimeField(auto_now_add=True)
+    
