9bffab15aeb5885ec2bf7df48046209a9734acf5
freyben <114809028+freyben@users.noreply.github.com>
2025-06-23 14:19:28 +0200

    Delete apps/homepage/models.py

--- apps/homepage/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class EmailSubscription(models.Model):
-    email = models.EmailField(unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.email
