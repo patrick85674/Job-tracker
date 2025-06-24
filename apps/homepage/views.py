f904e267e62a4a5bfba26f2609d7a55307057c48
YuliiaMykhailychenko <mykhailychenkoyuliia@gmail.com>
2025-06-24 13:29:24 +0200

    Add About, Contact, Privacy, Terms pages and update login +footer

--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -8,6 +8,18 @@ def homepage_view(request):
 def faq(request):
     return render(request, 'faq.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def terms(request):
+    return render(request, 'terms.html')
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
 
 def subscribe(request):
     if request.method == "POST":
